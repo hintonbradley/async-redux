116341decc5cb42d9842ad84ed1c00d8588408f9
hintonbradley <hintonbradley@gmail.com>
2020-06-07 13:47:52 -0700

    small updte

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,7 @@ import PostList from './PostList';
 function Home() {
 	return (
 		<div className="home ui container">
-			<h1 className="header">This is the Home page</h1>
+			<h1 className="header">- home page -</h1>
 			< PostList />
 		</div>
 	);
